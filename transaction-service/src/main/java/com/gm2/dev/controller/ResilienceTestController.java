27a4959dd0a177f62251e065dcd05949a6950727
Genilson Medeiros Martins <genilson.martins@virtus.ufcg.edu.br>
2022-12-16 10:52:04 -0300

    resilience4j

--- transaction-service/src/main/java/com/gm2/dev/controller/ResilienceTestController.java
+++ transaction-service/src/main/java/com/gm2/dev/controller/ResilienceTestController.java
@@ -4,6 +4,7 @@ import com.gm2.dev.dto.ResponseDto;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,18 +13,23 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("resilience")
-public class ResilienceTestController {
+public class ResilienceTestController extends BaseController {
 
     private Logger logger = LoggerFactory.getLogger(ResilienceTestController.class);
 
 
     @GetMapping
-    @Retry(name = "default")
-    public ResponseEntity<ResponseDto> get(){
+    @Retry(name = "account", fallbackMethod = "fallbackMethod")
+    public ResponseEntity<ResponseEntity<ResponseDto>> ResponseEntityget(){
         logger.info("Request recebido!");
         var response =
                 new RestTemplate().getForEntity("http://localhost:8000/customer-accountooooo",
                         ResponseDto.class);
-        return response;
+
+        return ResponseEntity.ok(response);
+    }
+
+    private ResponseEntity fallbackMethod(Exception error){
+        return getResponseError("Serviço indisponível", HttpStatus.OK);
     }
 }
